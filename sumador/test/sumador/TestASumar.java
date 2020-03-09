7cbbc28a1d6bbfa036751f57a18db613387b19fa
Gabriela Zavala <Propietario@pc-1559579848>
2020-03-09 23:11:29 +0100

    inicial

---
+++ sumador/test/sumador/TestASumar.java
@@ -0,0 +1,19 @@
+package sumador;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestASumar {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("No implementado aun");
+	}
+
+}
